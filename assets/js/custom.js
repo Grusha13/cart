3f0940d97095705ee44ccd88bfe4b082260a6a67
grusha13 <shahgrusha@gmail.com>
2025-02-24 19:18:55 +0530

    css changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,5 @@
 
   $(document).ready(function(){
-
-    var scrollTrigger = 500; 
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > scrollTrigger) {
-            $('#toTop').fadeIn();
-        } else {
-            $('#toTop').fadeOut();
-        }
-    });
-
-    $('#toTop').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1000);
-        return false;
-    });
-
   const formIds = ["#form_phone_1", "#form_phone_2", "#form_phone_3"];
     formIds.forEach(id => {
     const $input = $(id);
